bacbbab8b61c70d039990323283fafa0ae6457cc
quytmoiz146 <giangquyet386@gmail.com>
2026-03-19 23:00:20 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ bool lastInputState = 0;
 bool active = false;
 
 void Led_Buzzer();
+void Real_Time();
 
 void setup() {
   Serial.begin(9600);
@@ -46,11 +47,15 @@ void setup() {
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
-  RS485Serial.begin
+  pinMode(RS485_EN, OUTPUT);
+  digitalWrite(RS485_EN, LOW);
+
+  RS485Serial.begin(9600, SERIAL_8N1, 18, 17);
 }
 
 void loop() {
   Led_Buzzer();
+  Real_Time();
 }
 
 void Led_Buzzer(){
@@ -63,23 +68,38 @@ void Led_Buzzer(){
 
   if(active){
     currentTime = millis();
-    if(currentTime - startTime == 30000){
+    if(currentTime - startTime >= 30000){
       active = false;
       digitalWrite(LED, HIGH);
       digitalWrite(BUZZER, LOW);
       return;
     }
 
-    if(currentTime - lastBuzzerTime == 1000){
+    if(currentTime - lastBuzzerTime >= 1000){
       lastBuzzerTime = currentTime;
       buzzerState = !buzzerState;
       digitalWrite(BUZZER, buzzerState);
     }
 
-    if(currentTime - lastLedTime == 500){
+    if(currentTime - lastLedTime >= 500){
       lastLedTime = currentTime;
       ledState = !ledState;
       digitalWrite(LED, ledState);
     }
+  }
+}
+
+void Real_Time(){
+  unsigned long nowMillis = millis();
+  if(nowMillis - lastSend >= 1000){
+    lastSend = nowMillis;
+    DateTime now = rtc.now();
+    char data[30];
+    sprintf(data, "%02d:%02d:%02d %02d/%02d/%04d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
+    digitalWrite(RS485_EN, HIGH);
+    RS485Serial.println(data);
+    delay(10);
+    digitalWrite(RS485_EN, LOW);
+    Serial.println(data);
   }
 }
